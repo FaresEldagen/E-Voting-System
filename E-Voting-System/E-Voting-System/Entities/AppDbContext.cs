eb11b8180e2c8ce44a12b128467e39c2d027c4ef
Ahmed Albatrawy <albatrawyahmed@gmail.com>
2026-04-20 20:31:08 +0200

    Add EF Core, AppDbContext, and User entity setup
    Added Entity Framework Core dependencies and created AppDbContext with User DbSet. Introduced User entity with Id and nullable Vote properties. Set up initial entity configurations and added folder for future entity configurations.

---
+++ E-Voting-System/E-Voting-System/Entities/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Voting_System.Entities;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+        modelBuilder.Entity<User>().Property(u => u.Id)
+            .ValueGeneratedNever();
+        modelBuilder.Entity<User>().Property(u => u.Vote)
+            .HasColumnType("INT")
+            .IsRequired(false);
+    }
+}
+
